17e59086946b97ca4fadab1be6c83bf20ce06126
Connor McGlynn <connorappleid@gmail.com>
2018-06-25 16:43:18 +0000

    finished mario(more) pset1

--- pset1/mario-more.c
+++ pset1/mario-more.c
@@ -5,16 +5,20 @@ int main(void) {
 
     int height;
     do {
-        height = get_int("Height: ");
+        height = get_int("Height: "); // get height from user
     } while (height < 0 || height > 23);
 
     int i, j, k;
-    for (i = 2; i <= height; i++) { // make lines
+    for (i = 1; i <= height; i++) { // make lines
         for (j = 0; j < (height - i); j++) {
             printf(" "); // prints spaces
         }
         for (k = 0; k < i; k++) {
         printf ("#"); // prints hashes
+        }
+        printf ("  "); // divides the stairs
+        for (k = 0; k < i; k++) {
+        printf ("#");
         }
         printf("\n"); // creates newlines
     }
