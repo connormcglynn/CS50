b2cd6cb871dd7498e721b06d9133e905d5701a5a
Connor McGlynn <connorappleid@gmail.com>
2018-06-25 16:07:46 +0000

    finished pset1 hello.c

---
+++ in_class/noswitch.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)  {
+    char c = get_char("Answer: ");
+
+    if (c == 'Y' || c == 'y') {
+        printf("yes\n");
+    } else if (c == 'N' || c == 'n') {
+        printf("no\n");
+    }
+}
