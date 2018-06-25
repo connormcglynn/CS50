93446859e72a1d152d55d959475702ffe277aba5
Connor McGlynn <connorappleid@gmail.com>
2018-06-25 16:36:47 +0000

    finished mario(less)

---
+++ pset1/mario-less.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void) {
+
+    int height;
+    do {
+        height = get_int("Height: ");
+    } while (height < 0 || height > 23);
+
+    int i, j, k;
+    for (i = 1; i <= height; i++) { // make lines
+        for (j = 0; j < (height - i); j++) {
+            printf(" "); // prints spaces
+        }
+        for (k = 0; k < i; k++) {
+        printf ("#"); // prints hashes
+        }
+        printf("\n"); // creates newlines
+    }
+    return 0;
+}
