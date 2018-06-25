db5b7b9b523073335dc108e1e31f55e11f20f54e
Connor McGlynn <connorappleid@gmail.com>
2018-06-25 17:13:28 +0000

    adjusted mario(more) for check50 and style50

--- pset1/mario-more/mario.c
+++ pset1/mario-more/mario.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void) {
-
+int main(void)
+{
     int height;
-    do {
+    do
+    {
         height = get_int("Height: "); // get height from user
-    } while (height < 0 || height > 23);
+    }
+    while (height < 0 || height > 23);
 
     int i, j, k;
-    for (i = 1; i <= height; i++) { // make lines
-        for (j = 0; j < (height - i); j++) {
+    for (i = 1; i <= height; i++) // make lines
+    {
+        for (j = 0; j < (height - i); j++)
+        {
             printf(" "); // prints spaces
         }
-        for (k = 0; k < i; k++) {
-        printf ("#"); // prints hashes
+        for (k = 0; k < i; k++)
+        {
+            printf("#"); // prints hashes
         }
-        printf ("  "); // divides the stairs
-        for (k = 0; k < i; k++) {
-        printf ("#");
+        printf("  "); // divides the stairs
+
+        for (k = 0; k < i; k++)
+        {
+            printf("#");
         }
         printf("\n"); // creates newlines
     }
