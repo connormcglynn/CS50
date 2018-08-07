9cf19595877ce103be2d416938dcdbb5e9aebf96
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-08-07 16:28:04 +0000

    updated readme/test for pset5

--- pset5/speller/test.c
+++ pset5/speller/test.c
@@ -2,3 +2,13 @@
 
 // ** Remember to use help50 valgrind ./memory for debugging ** //
 
+// Create an array of linked lists, each by letters A through Z
+
+
+// RESOURCES
+
+// https://www.learn-c.org/en/Linked_lists
+// https://www.geeksforgeeks.org/linked-list-set-1-introduction/
+// https://www.tutorialspoint.com/data_structures_algorithms/hash_table_program_in_c.htm
+// https://www.quora.com/How-can-I-implement-a-dictionary-using-C
+// https://www.geeksforgeeks.org/hashing-set-1-introduction/
